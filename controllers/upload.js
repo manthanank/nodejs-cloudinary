d5a7ec6e9eeffbe2863032307b9ef0f0bb84b1cf
Manthan Ankolekar <manthan.ank46@gmail.com>
2023-12-23 12:29:20 +0530

    feat: updated

--- controllers/upload.js
+++ controllers/upload.js
@@ -10,7 +10,19 @@ exports.upload = async (req, res) => {
         crop: "scale",
       }
     );
+    console.log(myCloud);
     res.json({ success: true, message: "Upload Successful" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+exports.remove = async (req, res) => {
+  try {
+    const myCloud = cloudinary.v2.uploader.destroy(req.body.imageId);
+    console.log(myCloud);
+    res.json({ success: true, message: "Deleted Successful" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
--- routes/upload.js
+++ routes/upload.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { upload } = require('../controllers/upload.js');
+const { upload, remove } = require('../controllers/upload.js');
 
 router.post('/upload', upload);
+router.post('/remove', remove);
 
 module.exports = router;
